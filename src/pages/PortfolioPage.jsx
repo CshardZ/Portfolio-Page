af87c413a05bb14ba94330cb02453ecd8473f5de
CshardZ <vivekhubstrt@gmail.com>
2026-06-17 18:23:36 +0530

    chore: reset initial react project codebase and add alias

---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header"
+import Sidebar from "@/components/Sidebar"
+import Footer from "@/components/Footer"
+import Main from "@/components/Main"
+
+
+export default function PortfolioPage() {
+  return (
+    <main id='portfolio-page'>
+        <Header />
+        <Sidebar />
+        <Main />
+        <Footer />
+    </main>
+  )
+}
